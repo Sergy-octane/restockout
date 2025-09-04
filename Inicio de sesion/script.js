a3f8692fc37de18c17574f0bc1636d179632a923
Sergy Ferreira <sergyferreira153@gmail.com>
2025-09-03 19:53:08 -0500

    actualizacion

--- Inicio de sesion/script.js
+++ Inicio de sesion/script.js
@@ -1,15 +1,25 @@
 document.getElementById("loginForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    const email = document.getElementById("email").value;
-    const pass = document.getElementById("password").value;
-  
-    if (email && pass) {
-      alert("¡Login exitoso!");
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("password").value;
+  const mensajeLogin = document.getElementById("mensajeLogin");
+
+  if (email && pass) {
+    mensajeLogin.textContent = "✅ ¡Login exitoso!";
+    mensajeLogin.className = "mensaje-login exito";
+    mensajeLogin.style.display = "block";
+
+    // Redirige después de 1.5 segundos
+    setTimeout(() => {
       window.location.href = "restockout-dashboard/index.html";
-    } else {
-      alert("Correo o contraseña incorrectos.");
-    }
-  });
+    }, 1500);
+  } else {
+    mensajeLogin.textContent = "❌ Correo o contraseña incorrectos.";
+    mensajeLogin.className = "mensaje-login error";
+    mensajeLogin.style.display = "block";
+  }
+});
+
   // Botón de ayuda que abre WhatsApp
 document.getElementById("ayuda-btn").addEventListener("click", () => {
   const numero = "573058962619"; // 👉 reemplaza con tu número
